814aad5c16c11d0339bd56048932499677a31ad8
David Beneš <david@davidbenes.cz>
2026-02-09 17:10:39 +0100

    feat: Integrate a ChatGPT webview into the agent sidebar and enable the webview tag in Electron.

--- main.js
+++ main.js
@@ -8,7 +8,8 @@ function createWindow() {
         height: 800,
         webPreferences: {
             nodeIntegration: true,
-            contextIsolation: false
+            contextIsolation: false,
+            webviewTag: true
         }
     })
 
